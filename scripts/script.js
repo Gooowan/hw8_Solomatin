a42e5b71b20c7055b6632f5d8aac270e8075577e
BUNJEE <vsolomatin@kse.org.ua>
2023-10-03 14:41:56 +0300

    local storage works

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,33 @@
+class TodoItem {
+    constructor(text) {
+        this.text = text;
+        this.dateUpdated = new Date();
+        this.completed = false;
+    }
+}
+
+class TodoItemPremium extends TodoItem {
+    constructor(text, image) {
+        super(text);
+        this.image = image || null;
+    }
+}
+
+// Load tasks from local storage
+let tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+tasks = tasks.map(task => Object.assign(new TodoItem(), task));
+
+// Save tasks to local storage
+function saveTasksToLocalStorage() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
     const saveTaskBtn = document.getElementById('save-task-btn');
     const removeAllBtn = document.getElementById('remove-all-btn');
     const removeCompletedBtn = document.getElementById('remove-completed-btn');
-
-    let tasks = [];
 
     function createTaskElement(task, index) {
         const li = document.createElement('li');
@@ -15,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
         li.innerHTML = `
         <span class="checkbox" data-index="${index}">${task.completed ? '✅' : '❌'}</span>
         <span class="task-text" data-index="${index}">${task.text}</span>
-        <span class="days-to-deadline" data-index="${index}">${task.daysToDeadline} </span>
+        <span class="days-to-deadline" data-index="${index}">${task.daysToDeadline}</span>
         <input type="number" class="edit-deadline-input" data-index="${index}" style="display:none;" min="1" value="${task.daysToDeadline}">
         <button class="edit-deadline-btn" data-index="${index}">Edit Deadline</button>
         <button class="remove-btn" style="margin-top: 5px" data-index="${index}">Remove</button>
@@ -25,10 +47,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function renderTasks() {
         taskList.innerHTML = '';
-       tasks.sort((a, b) => a.daysToDeadline - b.daysToDeadline);
+        tasks.sort((a, b) => a.dateUpdated.getTime() - b.dateUpdated.getTime());
         tasks.forEach((task, index) => {
             taskList.appendChild(createTaskElement(task, index));
         });
+        saveTasksToLocalStorage();
     }
 
     function addOrUpdateTask(e, editIndex = null) {
@@ -38,26 +61,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (e.keyCode === 27) {
             taskInput.value = '';
-       saveTaskBtn.onclick = addOrUpdateTask;
+            saveTaskBtn.onclick = addOrUpdateTask;
             return;
         }
 
         if (editIndex !== null) {
             tasks[editIndex] = {
                 text: taskInput.value.trim(),
-                date: new Date(),
+                dateUpdated: new Date(),
                 completed: tasks[editIndex].completed
             };
         } else {
             tasks.unshift({
                 text: taskInput.value.trim(),
-                date: new Date(),
+                dateUpdated: new Date(),
                 completed: false,
                 daysToDeadline: "No deadline set"
             });
         }
         taskInput.value = '';
-       saveTaskBtn.onclick = addOrUpdateTask;
+        saveTaskBtn.onclick = addOrUpdateTask;
         renderTasks();
     }
 
@@ -85,18 +108,14 @@ document.addEventListener('DOMContentLoaded', function() {
     taskInput.addEventListener('keydown', addOrUpdateTask);
 
     removeAllBtn.addEventListener('click', function() {
-        if (!tasks.some(task => !task.completed) || confirm('Are you sure?')) {
-            tasks = [];
-            renderTasks();
-        }
+        tasks = [];
+        renderTasks();
     });
 
     removeCompletedBtn.addEventListener('click', function() {
         tasks = tasks.filter(task => !task.completed);
         renderTasks();
     });
-
-    renderTasks();
 
     taskList.addEventListener('click', function(e) {
         if (e.target.classList.contains('edit-deadline-btn')) {
@@ -109,15 +128,10 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Event listener to handle changes to the deadline input
-    // Event listener to handle changes to the deadline input
     taskList.addEventListener('blur', function(e) {
         if (e.target.classList.contains('edit-deadline-input')) {
             const index = e.target.dataset.index;
-
-            // Store the input value with the "days to deadline" text
             tasks[index].daysToDeadline = `${parseInt(e.target.value, 10)} days to deadline`;
-
             const spanElem = document.querySelector(`.days-to-deadline[data-index="${index}"]`);
             spanElem.textContent = tasks[index].daysToDeadline;
             e.target.style.display = "none";
@@ -126,8 +140,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }, true);
 
+    document.getElementById('clear-storage-btn').addEventListener('click', function() {
+        localStorage.clear();
+        tasks = [];
+        renderTasks();
+    });
 
+    document.getElementById('pick-todo-btn').addEventListener('click', function() {
+        if (tasks.length === 0) return;
+        const randomIndex = Math.floor(Math.random() * tasks.length);
+        const todos = document.querySelectorAll('#task-list li');
+        todos.forEach(todo => todo.classList.remove('active'));
+        todos[randomIndex].classList.add('active');
+    });
 
+    renderTasks();  // Initial render of tasks
 });
-
-
